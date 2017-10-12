b9692079c81809c3bd52406ec3b808187ad6eb1f
Clinton Siegel <ClintonSiegel@gmail.com>
2017-10-12 15:16:04 -0400

    Challenge complete

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -5,26 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
+    String BTC_LIT = "BTC";
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
 
     // Member Variables:
     TextView mPriceTextView;
+    String mCurrencyPair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,35 +49,72 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitcoin", "" + parent.getItemAtPosition(position));
+                mCurrencyPair = BTC_LIT + parent.getItemAtPosition(position);
+                Log.d("Bitcoin", "Currency Pair " + mCurrencyPair);
 
+              //RequestParams params = new RequestParams();
+                //params.put("market", "global");
+                //params.put("symbol", mCurrencyPair);
+                letsDoSomeNetWorking();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Bitcoin", "Nothing selected");
+
+            }
+        });
+    }
+
+    // Update the UI
+    private void updateUI(QuoteDataModel quote) {
+       mPriceTextView.setText(quote.getLast());
+        Log.d("Bitcoin", "mLast Text view: " + quote.getLast());
     }
 
     // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+    private void letsDoSomeNetWorking() {
+    AsyncHttpClient client = new AsyncHttpClient();
+      // client.get(BASE_URL, params, new JsonHttpResponseHandler() {
+      client.get(BASE_URL + mCurrencyPair, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("bitcoin", "JSON: " + response.toString());
+               QuoteDataModel quoteData = QuoteDataModel.fromJson(response);
+               updateUI(quoteData);
+            }
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+            }
+        });
     }
 
 
-}
+    public static class QuoteDataModel {
+    private String mLast;
+    public String getLast() {
+            return mLast;
+        }
+// TODO: Create a QuoteDataModel from a JSON:
+     public static QuoteDataModel fromJson(JSONObject jsonObject) {
+        try {
+             QuoteDataModel quoteData = new QuoteDataModel();
+             quoteData.mLast = jsonObject.getString("last");
+             Log.d("Bitcoin", "mLast Text view: " + quoteData.mLast);
+             return quoteData;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null; }
+       }
+    }
+   }
